b0a30fcb3b8b075910b460621d710e67eea32830
Anton Kudryashov <driver_90@list.ru>
2024-10-27 20:51:53 +0700

    first commit

---
+++ src/test/java/TasksTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TasksTest {
+
+    @Test
+    public void simpleTaskTest(){
+        SimpleTask test = new SimpleTask(2, "Задача");
+        test.matches("Задача");
+
+        Boolean expected = true;
+        Boolean actual = test.matches("Задача");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void epicTest(){
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic test = new Epic(2, subtasks);
+        test.matches("Молоко");
+
+        Boolean expected = true;
+        Boolean actual = test.matches("Молоко");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void meetingTest(){
+        Meeting test = new Meeting(555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда");
+        test.matches("Молоко");
+
+        Boolean expected = false;
+        Boolean actual = test.matches("Молоко");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
